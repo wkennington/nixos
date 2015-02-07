898450a84fcc9dc24988ea3edf87f5e0034e468b
William A. Kennington III <william@wkennington.com>
2015-02-07 12:56:45 -0800

    graphical/base: Undo the minimalistic settings needed for a graphical environment

--- graphical/base.nix
+++ graphical/base.nix
@@ -1,9 +1,13 @@
 { pkgs, lib, ... }:
 with lib;
 {
-  environment.systemPackages = [ pkgs.slock ];
-  fonts.fontconfig.enable = mkOverride 1 true;
+  # Undo minimalistic settings
+  fonts.fontconfig.enable = true;
   hardware.pulseaudio.enable = true;
+  security.pam.services.su.forwardXAuth = true;
+  sound.enable = true;
+
+  environment.systemPackages = [ pkgs.slock ];
   services = {
     kmscon.hwRender = true;
     pcscd.enable = true;
