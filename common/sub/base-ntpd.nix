362639c64c3afe828a3e7d59ea268cf357339a6f
William A. Kennington III <william@wkennington.com>
2015-05-11 14:16:10 -0700

    base-ntpd: Add target for synchronized time

--- common/sub/base-ntpd.nix
+++ common/sub/base-ntpd.nix
@@ -2,11 +2,22 @@
 {
   services = {
     ntp.enable = false;
-    openntpd.enable = true;
+    openntpd = {
+      enable = true;
+      extraOptions = "-s";
+    };
   };
 
   networking.firewall.extraCommands = ''
     ip46tables -A OUTPUT -m owner --uid-owner ntp -p udp --dport ntp -j ACCEPT
+  '';
+
+  systemd.targets.time-syncd = {};
+
+  systemd.services.openntpd.postStart = ''
+    if ntpctl -s status | grep -q 'clock synced'; then
+      systemctl start time-syncd.target
+    fi
   '';
 
   systemd.timers.check-ntpd = {
@@ -31,6 +42,9 @@
     path = with pkgs; [ gnugrep openntpd systemd ];
 
     script = ''
+      if ntpctl -s status | grep -q 'clock synced'; then
+        systemctl start time-syncd.target
+      fi
       if ntpctl -s all | grep -q 'not resolved'; then
         systemctl restart openntpd
       fi
