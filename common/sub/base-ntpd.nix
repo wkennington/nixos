ba1a174b71f78148fff84f716c363e2a3e821675
William A. Kennington III <william@wkennington.com>
2015-05-11 13:33:26 -0700

    base-ntp: Make sure names are resolved

--- common/sub/base-ntpd.nix
+++ common/sub/base-ntpd.nix
@@ -1,10 +1,38 @@
-{ ... }:
+{ pkgs, ... }:
 {
   services = {
     ntp.enable = false;
     openntpd.enable = true;
   };
+
   networking.firewall.extraCommands = ''
     ip46tables -A OUTPUT -m owner --uid-owner ntp -p udp --dport ntp -j ACCEPT
   '';
+
+  systemd.timers.check-ntpd = {
+    wantedBy = [ "timers.target" ];
+    requires = [ "openntpd.service" ];
+    bindsTo = [ "openntpd.service" ];
+
+    timerConfig = {
+      OnUnitActiveSec = 120;
+    };
+  };
+
+  systemd.services.check-ntpd = {
+    requires = [ "openntpd.service" ];
+    after = [ "openntpd.service" ];
+
+    serviceConfig = {
+      type = "oneshot";
+    };
+
+    path = with pkgs; [ gnugrep openntpd systemd ];
+
+    script = ''
+      if ntpctl -s all | grep -q 'not resolved'; then
+        systemctl restart openntpd
+      fi
+    '';
+  };
 }
