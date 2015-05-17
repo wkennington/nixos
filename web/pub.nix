da5704e46da88ba27fe8e4098365752cb5bae479
William A. Kennington III <william@wkennington.com>
2015-05-16 19:37:41 -0700

    web/pub: Add consul check

--- web/pub.nix
+++ web/pub.nix
@@ -1,8 +1,13 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
 let
+  calculated = (import ../common/sub/calculated.nix { inherit config lib; });
   vars = (import ../customization/vars.nix { inherit lib; });
 
   domain = "pub.${vars.domain}";
+  consulService = "pub";
+  consulDomain = "${consulService}.service.consul.${vars.domain}";
+  checkDomain = "${consulService}.${config.networking.hostName}.${vars.domain}";
+
   path = "/ceph/www-pub";
 in
 {
@@ -11,6 +16,8 @@ in
     server {
       listen 443;
       server_name ${domain};
+      server_name ${consulDomain};
+      server_name ${checkDomain};
 
       location / {
         root ${path};
@@ -32,6 +39,7 @@ in
     server {
       listen 80;
       server_name ${domain};
+      server_name ${consulDomain};
 
       location / {
         root ${path};
@@ -41,4 +49,24 @@ in
       error_page 500 502 503 504 /50x.html;
     }
   '';
+
+  environment.etc."consul.d/${consulService}.json".text = builtins.toJSON {
+    service = {
+      name = consulService;
+      port = 443;
+      checks = [
+        {
+          script = ''
+            export SSL_CERT_FILE="/etc/ssl/certs/ca-certificates.crt"
+            # TODO: Get a new cert and remove -k
+            if ${pkgs.curl}/bin/curl -k https://${checkDomain}/; then
+              exit 0
+            fi
+            exit 2 # Critical
+          '';
+          interval = "60s";
+        }
+      ];
+    };
+  };
 }
