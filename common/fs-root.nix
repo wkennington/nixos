0bd21d5e6189d7fc46a86f73ad3fca0094fa2a87
William A. Kennington III <william@wkennington.com>
2015-05-17 10:04:58 +0000

    fs-root: Add common derivation for common root filesystem options

---
+++ common/fs-root.nix
@@ -0,0 +1,23 @@
+{ config, ... }:
+{
+  boot = {
+    initrd.kernelModules = [ "fbcon" ];
+    loader = {
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot";
+      };
+      grub.efiSupport = true;
+    };
+  };
+
+  fileSystems = mkOrder 1 [
+    {
+      mountPoint = "/boot";
+      device = "${config.boot.loader.grub.device}-part2";
+      fsType = "vfat";
+      options = "defaults,noatime";
+      neededForBoot = true;
+    }
+  ];
+}
