ea47222591da647a6ec69ddc8fdeb6108b559214
William A. Kennington III <william@wkennington.com>
2015-06-09 01:24:12 -0700

    dhcpcd: Don't assign ip4ll addresses

--- common/sub/base-networking.nix
+++ common/sub/base-networking.nix
@@ -7,26 +7,33 @@ let
   hasWanIf = config.networking.interfaces ? "wan";
 in
 {
-  networking = mkIf (!calculated.iAmRemote) {
-    defaultGateway = mkDefault (if hasWanIf then null else calculated.myGatewayIp4);
+  networking = mkMerge [
+    {
+      dhcpcd.extraConfig = ''
+        noipv4ll
+      '';
+    }
+    (mkIf (!calculated.iAmRemote) {
+      defaultGateway = mkDefault (if hasWanIf then null else calculated.myGatewayIp4);
 
-    interfaces = {
-      lan = { };
-    } // listToAttrs (flip map calculated.myNetData.vlans (vlan:
-      nameValuePair vlan {
-        ip4 = mkDefault [ { address = calculated.myInternalIp4; prefixLength = 24; } ];
-      }
-    ));
+      interfaces = {
+        lan = { };
+      } // listToAttrs (flip map calculated.myNetData.vlans (vlan:
+        nameValuePair vlan {
+          ip4 = mkDefault [ { address = calculated.myInternalIp4; prefixLength = 24; } ];
+        }
+      ));
 
-    vlans = listToAttrs (flip map calculated.myNetData.vlans (vlan:
-      nameValuePair vlan {
-        id = vars.internalVlanMap.${vlan};
-        interface = "lan";
-      }
-    ));
+      vlans = listToAttrs (flip map calculated.myNetData.vlans (vlan:
+        nameValuePair vlan {
+          id = vars.internalVlanMap.${vlan};
+          interface = "lan";
+        }
+      ));
 
-    useDHCP = false;
+      useDHCP = false;
 
-    nameservers = if hasWanIf then [ "8.8.4.4" "8.8.8.8" ] else calculated.myGatewaysIp4;
-  };
+      nameservers = if hasWanIf then [ "8.8.4.4" "8.8.8.8" ] else calculated.myGatewaysIp4;
+    })
+  ];
 }
