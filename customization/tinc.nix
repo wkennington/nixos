36fe41e8bc097ec4d775ec205effff36be94b671
William A. Kennington III <william@wkennington.com>
2015-07-05 15:12:42 -0700

    customization: Fix newton

--- customization/tinc.nix
+++ customization/tinc.nix
@@ -44,7 +44,7 @@ with lib;
         Ed25519PublicKey = kKcEmjbD+1Fx8llu6xlAQsBiuSmb2wJp8PzhAnGtezI
       '';
       newton = ''
-        Address = newton.wak.ioa
+        Address = newton.wak.io
         Ed25519PublicKey = Y3l5+f/+GyOKcA99pXN5h2/DY5eYWYGSzVylKlv10lB
         Subnet = 10.2.0.0/16
       '';
