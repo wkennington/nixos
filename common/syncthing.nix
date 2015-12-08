ada944c48f552f2a224ec58f41b0aa20720d829c
William A. Kennington III <william@wkennington.com>
2015-12-08 12:47:59 -0800

    syncthing: Add

---
+++ common/syncthing.nix
@@ -0,0 +1,16 @@
+{ ... }:
+{
+  networking.firewall.extraCommands = ''
+    # Allow input to the sync port
+    ip46tables -A INPUT -p tcp --dport 22000 -j ACCEPT
+    ip46tables -A INPUT -p udp --dport 21027 -j ACCEPT
+
+    # Allow privileged users to access syncthing's webui
+    ip46tables -A OUTPUT -o lo --dport 8384 -j ACCEPT
+  '';
+  services.syncthing.enable = true;
+  users.extraUsers.syncthing = {
+    description = "Syncthing service user";
+    isSystemUser = true;
+  };
+}
