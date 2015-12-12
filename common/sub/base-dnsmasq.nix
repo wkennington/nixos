b56c3774441c8fb5d8b232daca7e85324004d37b
William A. Kennington III <william@wkennington.com>
2015-12-11 16:07:53 -0800

    dnsmasq: Enable dnssec

--- common/sub/base-dnsmasq.nix
+++ common/sub/base-dnsmasq.nix
@@ -1,4 +1,23 @@
-{ config, ... }:
+{ config, lib, pkgs, ... }:
+let
+  trustAnchor = lib.readFile (pkgs.stdenv.mkDerivation {
+    name = "dnssec-anchor-dnsmasq";
+    builder = pkgs.writeText "builder.sh" ''
+      ${pkgs.gawk}/bin/awk '
+      {
+        if (/Domain:/) { domain=$2; }
+        if (/Id:/) { id=$2; }
+        if (/Algorithm:/) { algorithm=$2; }
+        if (/HashType:/) { hashtype=$2; }
+        if (/Hash:/) { hash=$2; }
+      }
+      END {
+        print domain "," id "," algorithm "," hashtype "," hash;
+      }
+      ' "${pkgs.dnssec-root}/share/dnssec/icann-root.txt" > $out
+    '';
+  });
+in
 {
   networking.firewall.extraCommands = ''
     # Allow dnsmasq to access dns servers
@@ -22,6 +41,12 @@
       no-hosts
       addn-hosts=${config.environment.etc.hosts.source}
 
+      # Enable Dnssec
+      dnssec
+      trust-anchor=${trustAnchor}
+      dnssec-check-unsigned
+      dnssec-timestamp=/var/lib/dnsmasq/timestamp
+
       interface=lo
       no-dhcp-interface=lo
 
@@ -30,4 +55,10 @@
       expand-hosts
     '';
   };
+
+  systemd.services.dnsmasq.preStart = ''
+    mkdir -p /var/lib/dnsmasq
+    chmod 0700 /var/lib/dnsmasq
+    chown dnsmasq /var/lib/dnsmasq
+  '';
 }
