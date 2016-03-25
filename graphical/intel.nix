e76985d1c5920364f718edfcc3cbd944663a550d
William A. Kennington III <william@wkennington.com>
2016-03-24 21:19:12 -0700

    Use s3tc instead of s2tc

--- graphical/intel.nix
+++ graphical/intel.nix
@@ -1,6 +1,7 @@
 { config, pkgs, ... }:
 {
   imports = [ ./base.nix ];
+  hardware.opengl.s3tcSupport = true;
   services.kmscon.enable = true;
   services.xserver = {
     vaapiDrivers = [ pkgs.vaapi-intel ];
