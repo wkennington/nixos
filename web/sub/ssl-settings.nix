f10ebbcd7dd81201299e1d69ff6c5a0d219b0184
William A. Kennington III <william@wkennington.com>
2016-04-04 16:29:55 -0700

    nginx: Make sure we have a resolver

--- web/sub/ssl-settings.nix
+++ web/sub/ssl-settings.nix
@@ -14,5 +14,6 @@
   ssl_session_timeout 10m;
   ssl_stapling on;
   ssl_stapling_verify on;
+  resolver 127.0.0.1;
   add_header Strict-Transport-Security "max-age=31536000; includeSubDomains; preload" always;
 ''
