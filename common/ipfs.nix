40539795cf3b54515ef300ae1921cc42f93778f2
William A. Kennington III <william@wkennington.com>
2016-04-03 20:52:40 -0700

    ipfs: Allow local process access to the gateway

--- common/ipfs.nix
+++ common/ipfs.nix
@@ -7,6 +7,11 @@
   networking.firewall = {
     allowedTCPPorts = [ 4001 ];
     allowedUDPPorts = [ 4001 ];
+
+    extraCommands = ''
+      # Allow all other processes to acccess the gateway
+      ip46tables -A OUTPUT -o lo -p tcp --dport 8001 -j ACCEPT
+    '';
   };
 
   services.ipfs = {
