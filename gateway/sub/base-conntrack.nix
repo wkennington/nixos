6c178730b65ff0aba2ec76aba0c31cb41393d838
William A. Kennington III <william@wkennington.com>
2016-04-05 18:13:19 -0700

    conntrack: Using multicast now

--- gateway/sub/base-conntrack.nix
+++ gateway/sub/base-conntrack.nix
@@ -10,15 +10,10 @@ in
 {
   environment.systemPackages = [ pkgs.conntrack-tools ];
 
-  networking.firewall.extraCommands = ''
-    ip46tables -I INPUT -p udp --dport 3780 -i tlan -j ACCEPT
-  '';
-
   services.conntrackd = {
     enable = true;
     interface = "tlan";
     localAddress = calculated.internalIp4 config.networking.hostName "tlan";
-    remoteAddresses = map (n: calculated.internalIp4 n "tlan") otherGateways;
     ignoreAddresses = [
       calculated.myVpnIp4
     ] ++ map (vlan: calculated.internalIp4 config.networking.hostName vlan) calculated.myNetData.vlans;
