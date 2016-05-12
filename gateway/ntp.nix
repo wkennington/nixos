76786e9b790202e481440a2d7cbd6d7bc02dd030
William A. Kennington III <william@wkennington.com>
2016-05-11 22:00:57 -0700

    Fix ntp

--- gateway/ntp.nix
+++ gateway/ntp.nix
@@ -3,8 +3,8 @@ with lib;
 {
   networking.firewall.extraCommands =
     flip concatMapStrings config.myNatIfs (n: ''
-      ip46tables -A INPUT -i ${n} -p udp --dport domain -j ACCEPT
-      ip46tables -A INPUT -i ${n} -p tcp --dport domain -j ACCEPT
+      ip46tables -A INPUT -i ${n} -p udp --dport ntp -j ACCEPT
+      ip46tables -A INPUT -i ${n} -p tcp --dport ntp -j ACCEPT
     '');
 
   services.openntpd.extraConfig = ''
