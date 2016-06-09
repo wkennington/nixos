1b43b7a88334828a1f05633dc233b8f8d62ccef7
William A. Kennington III <william@wkennington.com>
2016-06-09 00:58:46 -0700

    knot: Actually fix routing this time

--- services/authdns.nix
+++ services/authdns.nix
@@ -17,9 +17,8 @@ in
     ip46tables -t nat -A PREROUTING -i wan -p tcp --dport 53 -j REDIRECT --to-port 1153
 
     # Rewrite traffic to replicate from the correct ip
-    #iptables -t mangle -A OUTPUT -m owner --uid-owner knot -p tcp --dport 53 -j MARK --set-mark 0x20
-    #iptables -t mangle -A OUTPUT -m owner --uid-owner knot -p udp --dport 53 -j MARK --set-mark 0x20
-    #iptables -t nat -A OUTPUT -i ${calculated.myInternalIp4}
+    iptables -t mangle -A OUTPUT -m owner --uid-owner knot -j MARK --set-mark 0x20
+    iptables -t nat -A POSTROUTING -m mark --mark 0x20 -j SNAT --to-source ${calculated.myInternalIp4}
   '';
 
   networking.localCommands = ''
