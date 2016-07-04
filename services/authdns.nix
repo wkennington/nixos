e748d44c3ded206fbff6effcd04c710ecb7c1c9b
William A. Kennington III <william@wkennington.com>
2016-07-04 11:47:22 -0700

    Fix local connections to knot

--- services/authdns.nix
+++ services/authdns.nix
@@ -17,8 +17,12 @@ in
     ip46tables -t nat -A PREROUTING -i wan -p tcp --dport 53 -j REDIRECT --to-port 1153
 
     # Rewrite traffic to replicate from the correct ip
-    iptables -t mangle -A OUTPUT -m owner -p tcp --dport 53 --uid-owner knot -j MARK --set-mark 0x20
-    iptables -t mangle -A OUTPUT -m owner -p udp --dport 53 --uid-owner knot -j MARK --set-mark 0x20
+    iptables -t mangle -X nixos-knot-output || true
+    iptables -t mangle -N nixos-knot-output
+    iptables -t mangle -A nixos-knot-output -o lo -j RETURN
+    iptables -t mangle -A nixos-knot-output -j MARK --set-mark 0x20
+    iptables -t mangle -A OUTPUT -m owner -p tcp --dport 53 --uid-owner knot -j nixos-knot-output
+    iptables -t mangle -A OUTPUT -m owner -p udp --dport 53 --uid-owner knot -j nixos-knot-output
     iptables -t nat -A POSTROUTING -m mark --mark 0x20 -j SNAT --to-source ${calculated.myInternalIp4}
   '';
 
