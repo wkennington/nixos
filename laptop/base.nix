92b2bb2213177ac399e1de86be001ae5186d4ff2
William A. Kennington III <william@wkennington.com>
2016-08-30 17:18:55 -0700

    laptop: More power saving features

--- laptop/base.nix
+++ laptop/base.nix
@@ -23,5 +23,7 @@
   services.udev.extraRules = ''
     ACTION=="add", SUBSYSTEM=="pci", ATTR{power/control}="auto"
     ACTION=="add", SUBSYSTEM=="usb", TEST=="power/control", ATTR{power/control}="auto"
+    ACTION=="add", SUBSYSTEM=="scsi_host", TEST=="link_power_management_policy", ATTR{link_power_management_policy}="min_power"
+    ACTION=="add", SUBSYSTEM=="module", TEST=="parameters/power_save", ATTR{parameters/power_save}="1"
   '';
 }
