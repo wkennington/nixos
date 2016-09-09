e562cbab2b764ac6d837e88b412d5761b13f2150
William A. Kennington III <william@wkennington.com>
2016-09-08 22:33:59 -0700

    syncthing: Allow local callouts

--- common/syncthing.nix
+++ common/syncthing.nix
@@ -5,8 +5,12 @@
     ip46tables -A INPUT -p tcp --dport 22000 -j ACCEPT
     ip46tables -A INPUT -p udp --dport 21027 -j ACCEPT
 
+    # Allow local syncthings to access each other
+    ip46tables -A OUTPUT -m owner --uid-owner syncthing -p tcp --dport 22000 -j ACCEPT
+    ip46tables -A OUTPUT -m owner --uid-owner syncthing -p udp --dport 21027 -j ACCEPT
+
     # Allow privileged users to access syncthing's webui
-    ip46tables -A OUTPUT -o lo -p tcp --dport 8384 -j ACCEPT
+    ip46tables -A OUTPUT -m owner --gid-owner wheel -o lo -p tcp --dport 8384 -j ACCEPT
   '';
   services.syncthing.enable = true;
 }
