d2fa2320002bd3a3f5e2384688c71735ca669caf
William A. Kennington III <william@wkennington.com>
2016-10-10 01:47:30 -0700

    Enable gluster to talk to other glusters

--- core/glusterfs.nix
+++ core/glusterfs.nix
@@ -1,6 +1,12 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  calculated = (import ../common/sub/calculated.nix { inherit config lib; });
+in
 {
   environment.systemPackages = [
     pkgs.glusterfs
   ];
+  networking.firewall.extraCommands = ''
+    iptables -A INPUT -p tcp --dport 24007 -s ${calculated.myInternalIp4Net} -j ACCEPT
+  '';
 }
