062dd23351a88e94397199afe60e165d32546dd9
William A. Kennington III <william@wkennington.com>
2016-11-06 23:04:53 -0800

    wireguard: Update module to generate files

--- common/wireguard.nix
+++ common/wireguard.nix
@@ -1,12 +1,33 @@
-{ lib, ... }:
+{ config, lib, pkgs, ... }:
 
 with lib;
 let
   vars = (import ../customization/vars.nix { inherit lib; });
+  wgConfig = (import ../customization/wireguard.nix { });
   calculated = (import ../common/sub/calculated.nix { inherit config lib; });
 
   # Make sure this is only used for wireguard and nothing else
-  port = "655";
+  port = 655;
+
+  # Files needed to build the configuration
+  secretDir = "/conf/wireguard";
+  keyFile = "${secretDir}/${vars.domain}.key";
+  pskFile = "${secretDir}/${vars.domain}.psk";
+
+  confFileIn = pkgs.writeText "wg.conf.in" (''
+    [Interface]
+    PrivateKey = @KEY@
+    PresharedKey = @PSK@
+    ListenPort = ${toString port}
+  '' + concatStrings (flip mapAttrsToList wgConfig.hosts (host: { publicKey, endpoint ? null }: ''
+    
+    [Peer]
+    PublicKey = ${publicKey}
+    AllowedIPs = ${calculated.vpnIp4 host}/32
+    AllowedIPs = ${calculated.vpnIp6 host}/128
+  '' + optionalString (endpoint != null) ''
+    Endpoint = ${endpoint}
+  '')));
 in
 {
   imports = [
@@ -16,7 +37,65 @@ in
   networking.wgs."${vars.domain}.vpn".configFile = "/etc/wg.${vars.domain}.conf";
   
   networking.firewall.extraCommands = ''
-    ip46tables -A INPUT -p udp --dport ${port} -j ACCEPT
-    ip46tables -A OUTPUT -p udp --dport ${port} -j ACCEPT
+    ip46tables -A INPUT -p udp --dport ${toString port} -j ACCEPT
+    ip46tables -A OUTPUT -p udp --dport ${toString port} -j ACCEPT
   '';
+
+  systemd.services."build-wg.${vars.domain}.conf" = {
+    serviceConfig = {
+      Type = "oneshot";
+      Restart = "no";
+    };
+
+    requiredBy = [
+      "network-dev-${vars.domain}.vpn.service"
+    ];
+    before = [
+      "network-dev-${vars.domain}.vpn.service"
+    ];
+
+    path = [
+      pkgs.coreutils
+      pkgs.gawk
+      pkgs.gnused
+      pkgs.wireguard
+    ];
+
+    script = ''
+      cleanup() {
+        rm -f "$TMP"
+        trap - EXIT
+        exit 0
+      }
+      trap cleanup EXIT ERR INT QUIT PIPE TERM
+      TMP="$(mktemp -p "/dev/shm")"
+      chmod 0600 "$TMP"
+      cat "${confFileIn}" >"$TMP"
+
+      if ! test -e "${keyFile}" || ! wg pubkey <"${keyFile}" >/dev/null 2>&1; then
+        exit 2
+      fi
+      export KEY="$(cat "${keyFile}")"
+      awk -i inplace '
+        {
+          gsub(/@KEY@/, ENVIRON["KEY"]);
+          print;
+        }
+      ' "$TMP"
+
+      if test -e "/conf/wireguard/${vars.domain}.psk"; then
+        export PSK="$(cat "${pskFile}")"
+        awk -i inplace '
+          {
+            gsub(/@PSK@/, ENVIRON["PSK"]);
+            print;
+          }
+        ' "$TMP"
+      else
+        sed -i '/@PSK@/d' "$TMP"
+      fi
+
+      mv "$TMP" "/etc/wg.${vars.domain}.conf"
+    '';
+  };
 }
---
+++ customization/wireguard.nix
@@ -0,0 +1,14 @@
+{ ... }:
+
+{
+  hosts = {
+    atomic = {
+      publicKey = "nSo/TBiHE+TQ6I1XyU7Pa7Oit9/eQQTDm1LxxDT0DUE=";
+      endpoint = "atomic.wak.io:656";
+    };
+    nevada = {
+      publicKey = "f7m+rqpaUOHDvH/Wwb2SXBm0HGG0ET1pifUmEvnCvnI=";
+      endpoint = "nevada.slan.wak.io:656";
+    };
+  };
+}
