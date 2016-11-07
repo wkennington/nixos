30c35b2fa5f1fa656ecb2feda58ede2ae3909608
William A. Kennington III <william@wkennington.com>
2016-11-07 01:11:16 -0800

    wireguard: Add more hosts

--- customization/wireguard.nix
+++ customization/wireguard.nix
@@ -5,6 +5,22 @@
     atomic = {
       publicKey = "nSo/TBiHE+TQ6I1XyU7Pa7Oit9/eQQTDm1LxxDT0DUE=";
     };
+    newton = {
+      publicKey = "cBrmihVXtv8HeqUwXZ77q1ciXJkSSPXBeJ8ih14OpD0=";
+      endpoint = "newton.wak.io:656";
+    };
+    page = {
+      publicKey = "rJ4hsXQfUX4yatWCSgunbE7q3Zkmixt2RYpKlu70jyk=";
+      endpoint = "page.wak.io:656";
+    };
+    quest = {
+      publicKey = "xvX4tER03tr6X0xyOh4NsLL617y4eJQwabZ+OAnw5xo=";
+      endpoint = "quest.wak.io:656";
+    };
+    ferrari = {
+      publicKey = "nD2YwYj+9AECqPS/InmiHOEhhN0VKUQYY9MBipXjS3U=";
+      endpoint = "ferrari.slan.wak.io:656";
+    };
     atlas = {
       publicKey = "Z2daI/c0gwn9aNQondelzTSg3jRCHRGypMDEICXbak8=";
       endpoint = "atlas.slan.wak.io:656";
