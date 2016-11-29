9ba2464df746cda5ae6d32d1c8359323dc6a51c2
William A. Kennington III <william@wkennington.com>
2016-11-29 15:42:30 -0800

    Don't masquerade wg connections anymore

--- gateway/sub/base-firewall.nix
+++ gateway/sub/base-firewall.nix
@@ -26,9 +26,9 @@ in
 
     # Masquerade all vpn connections
     # If we hit a node with vpn support we might have asymmetric routing otherwise.
-    iptables -t mangle -A PREROUTING -s "${vars.vpn.subnet4}0/24" -j MARK --set-mark 0x11
-    iptables -t mangle -A PREROUTING -s "${vars.vpn.remote4}0/24" -j MARK --set-mark 0x11
-    iptables -t nat -A POSTROUTING -m mark --mark 0x11 -j MASQUERADE
+    #iptables -t mangle -A PREROUTING -s "${vars.vpn.subnet4}0/24" -j MARK --set-mark 0x11
+    #iptables -t mangle -A PREROUTING -s "${vars.vpn.remote4}0/24" -j MARK --set-mark 0x11
+    #iptables -t nat -A POSTROUTING -m mark --mark 0x11 -j MASQUERADE
 
     # Masquerade all public connections
     iptables -t nat -A POSTROUTING -o wan -m mark --mark 0x10 -j ${natval}
