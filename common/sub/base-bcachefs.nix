0d06b8a89e547f35e5b0a09be245f9ad56348669
William A. Kennington III <william@wkennington.com>
2016-12-12 14:56:18 -0800

    Use bcache testing

--- common/sub/base-bcachefs.nix
+++ common/sub/base-bcachefs.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 {
-  boot.kernelPackages = lib.mkOverride 0 pkgs.linuxPackages_bcache;
+  boot.kernelPackages = lib.mkOverride 0 pkgs.linuxPackages_bcache-testing;
 
   environment.systemPackages = [
     pkgs.bcache-tools_dev
