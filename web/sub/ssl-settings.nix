4a742b64054c8e20e90a7b90d594c1d32a8d7dc8
William A. Kennington III <william@wkennington.com>
2016-12-20 20:54:37 -0800

    nginx-ssl: Add chacha as a cipher option

--- web/sub/ssl-settings.nix
+++ web/sub/ssl-settings.nix
@@ -2,7 +2,7 @@
 ''
   ssl on;
   ssl_protocols TLSv1.2;
-  ssl_ciphers EECDH+AESGCM;
+  ssl_ciphers EECDH+CHACHA20:EECDH+AESGCM;
   ssl_ecdh_curve secp384r1;
   ssl_prefer_server_ciphers on;
   ssl_certificate /conf/ssl/${domain}.crt;
