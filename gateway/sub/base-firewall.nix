b0335b6ff06cf29269c3093dfd08c47bd2d5abe3
William A. Kennington III <william@wkennington.com>
2017-04-16 23:17:24 -0700

    gateway: Add ipv6 nat

--- gateway/sub/base-firewall.nix
+++ gateway/sub/base-firewall.nix
@@ -8,6 +8,11 @@ let
     outboundOrNull = calculated.myNetMap.pub4MachineMap.outbound or null;
     pub4OrNull = calculated.myNetMap.pub4 or null;
   in if outboundOrNull == null || pub4OrNull == null then "MASQUERADE" else "SNAT --to ${pub4OrNull}${toString outboundOrNull}";
+
+  nat6val = let
+    outboundOrNull = calculated.myNetMap.pub6MachineMap.outbound or null;
+    pub6OrNull = calculated.myNetMap.pub6 or null;
+  in if outboundOrNull == null || pub6OrNull == null then "MASQUERADE" else "SNAT --to ${pub6OrNull}${toString outboundOrNull}";
 in
 {
   networking.firewall.extraCommands = mkOrder 2 ''
@@ -22,6 +27,7 @@ in
 
     # Masquerade all private connections
     iptables -t mangle -A PREROUTING -m set --match-set private src -j MARK --set-mark 0x10
+    ip6tables -t mangle -A PREROUTING -m set --match-set private6 src -j MARK --set-mark 0x10
     #iptables -t nat -A POSTROUTING -m mark --mark 0x10 -j MASQUERADE
 
     # Masquerade all vpn connections
@@ -33,6 +39,8 @@ in
     # Masquerade all public connections
     iptables -t nat -A POSTROUTING -o wan -m mark --mark 0x10 -j ${natval}
     iptables -t nat -A POSTROUTING -o gwan -m mark --mark 0x10 -j ${natval}
+    ip6tables -t nat -A POSTROUTING -o wan -m mark --mark 0x10 -j ${nat6val}
+    ip6tables -t nat -A POSTROUTING -o gwan -m mark --mark 0x10 -j ${nat6val}
 
     # Allow access to servers
     ${concatStrings (map (n: ''
