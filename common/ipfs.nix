4a39b75a4a92418d498f62f1302073b33169a6ca
William A. Kennington III <william@wkennington.com>
2017-09-27 22:17:48 -0700

    ipfs: Make public boxes specify their ips directly to avoid conflict

--- common/ipfs.nix
+++ common/ipfs.nix
@@ -1,4 +1,30 @@
-{ pkgs, ... }:
+{ config, lib, pkgs, ... }:
+let
+  inherit (lib)
+    concatLists
+    flip
+    optionals;
+
+  calculated = (import ../common/sub/calculated.nix { inherit config lib; });
+
+  addresses = optionals (calculated.myPublicIp4 != null) [
+    "/ip4/${calculated.myPublicIp4}"
+  ] ++ optionals (calculated.myPublicIp6 != null) [
+    "/ip6/${calculated.myPublicIp6}"
+  ];
+
+  addresses' =
+    if addresses != [] then
+      addresses
+    else
+      [ "/ip4/0.0.0.0" "/ip6/::" ];
+
+  swarm = concatLists (flip map addresses' (n: [
+    "${n}/tcp/4001"
+  ] ++ optionals (config.services.ipfs.utp) [
+    "${n}/udp/4001/utp"
+  ]));
+in
 {
   environment.systemPackages = with pkgs; [
     ipfs
@@ -16,5 +42,12 @@
 
   services.ipfs = {
     enable = true;
+    extraAttrs =  {
+      Addresses = {
+        Swarm = swarm;
+        API = "/ip4/127.0.0.1/tcp/5001";
+        Gateway = "/ip4/127.0.0.1/tcp/8001";
+      };
+    };
   };
 }
