5b9ef152ac0f97eb2001e8889828e6015d147e0c
William A. Kennington III <william@wkennington.com>
2017-12-22 16:09:52 -0800

    intel: Remove s3tc as it is baked in

--- graphical/intel.nix
+++ graphical/intel.nix
@@ -1,7 +1,6 @@
 { config, pkgs, ... }:
 {
   imports = [ ./base.nix ];
-  hardware.opengl.s3tcSupport = true;
   services.kmscon.enable = false;
   services.xserver = {
     vaapiDrivers = [ pkgs.intel-vaapi-driver ];
