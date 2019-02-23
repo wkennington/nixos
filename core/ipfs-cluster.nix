bc8ca8d14b1f7905b10fef6a31cd9d124eb33133
William A. Kennington III <william@wkennington.com>
2019-02-22 20:17:53 -0800

    ipfs-cluster: Firewall

--- core/ipfs-cluster.nix
+++ core/ipfs-cluster.nix
@@ -7,5 +7,9 @@
   networking.firewall.extraCommands = ''
     # Allow communicating with the local ipfs daemon
     ip46tables -A OUTPUT -m owner --uid-owner ipfs-cluster -o lo -p tcp --dport 5001 -j ACCEPT
+
+    # Allow ipfs-cluster to communicate with other ipfs-clusters
+    ip46tables -A INPUT -p tcp --dport 9096 -j ACCEPT
+    ip46tables -A OUTPUT -m owner --uid-owner ipfs-cluster -p tcp --dport 9096 -j ACCEPT
   '';
 }
