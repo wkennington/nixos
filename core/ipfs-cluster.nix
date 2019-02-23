3e072b0a453d9b1681b533f446f396760ea0ded3
William A. Kennington III <william@wkennington.com>
2019-02-22 20:06:16 -0800

    ipfs-cluster: Fixes

--- core/ipfs-cluster.nix
+++ core/ipfs-cluster.nix
@@ -1,6 +1,11 @@
-{  ... }:
+{ pkgs, ... }:
 {
+  environment.systemPackages = [
+    pkgs.ipfs-cluster
+  ];
   services.ipfs-cluster.enable = true;
   networking.firewall.extraCommands = ''
+    # Allow communicating with the local ipfs daemon
+    ip46tables -A OUTPUT -m owner --uid-owner ipfs-cluster -o lo -p tcp --dport 5001 -j ACCEPT
   '';
 }
